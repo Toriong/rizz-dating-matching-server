ce973513638dcdde3146dc0be13a1eea2d0268a6
Toriong <gtorion97@gmail.com>
2023-06-07 21:07:19 -0500

    creating service for inserting rejected user into the db

--- src/models/RejectedUser.ts
+++ src/models/RejectedUser.ts
@@ -1,49 +1,37 @@
-import mongoose from 'mongoose';
+import Mongoose from 'mongoose';
 
-const { Schema, Model, Document }  = mongoose;
-type DateFunction = () => number;
-
-interface ExpireAtSchema extends Schema<any> {
-  type: Date;
-  default: DateFunction;
+type NumberFn = () => number;
+interface ExpireAtInterface {
+    type: Date;
+    default: NumberFn
 }
-
-interface RejectedUserDocument extends Document {
-  _id: string;
-  rejectedUserId: string;
-  reason: string;
-  expireAt: ExpireAtSchema;
-}
-
-interface MongooseModels {
-  rejectedUsers: mongoose.Model<RejectedUserDocument>;
-}
-
-const models: MongooseModels = mongoose.models;
-
+interface RejectedUserInterface {
+    _id: string;
+    rejectedUserId: string;
+    reason?: string;
+    expireAt?: ExpireAtInterface
     
+}
 
+const { Schema, models, model } = Mongoose;
+let _RejectedUser;
 
-// GOAL #1: create a test that will add the rejected user to the RejectedUsers collection 
+if (!models.RejectedUsers) {
+    const milisecondsInADay = 86_400_000;
+    const ExpireAtSchema = new Schema<ExpireAtInterface>({
+        type: Date,
+        default: () => Date.now() + (milisecondsInADay * 10)
+    })
+    _RejectedUser = new Schema<RejectedUserInterface>({
+        // the id will be the user id of the rejector 
+        _id: String,
+        rejectedUserId: String,
+        reason: String,
+        expireAt: ExpireAtSchema
 
-// GOAL #2: write the code that will store the user into the database
+    }, { timestamps: true })
+}
 
-// GOAL #3: create a function that will send the id of the rejected user and the time (current time in miliseconds + 10 days in miliseconds) in which the user document will expire 
+const RejectedUser = models.RejectedUsers || model<RejectedUserInterface>('RejectedUsers', _RejectedUser);
 
-
-
-const MILISECONDS_IN_A_DAY = 86400000;
-
-const ExpireAtSchema = new mongoose.Schema({
-    type: Date,
-    default: () => Date.now() + (MILISECONDS_IN_A_DAY * 10)
-})
-
-const RejectedUser = new mongoose.Schema({
-    // the id will be the user id of the rejector 
-    _id: String,
-    rejectedUserId: String,
-    reason: String,
-    expireAt: ExpireAtSchema
-
-}, { timestamps: true })
+export { RejectedUser }
