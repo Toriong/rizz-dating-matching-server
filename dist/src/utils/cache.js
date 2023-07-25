e3e599b0d453839d449a3ee76f228ca18efcd010
Toriong <gtorion97@gmail.com>
2023-07-25 16:25:58 -0500

    using a class for the cache

--- dist/src/utils/cache.js
+++ dist/src/utils/cache.js
@@ -4,4 +4,23 @@ const cache = (() => {
     console.log('Cache initialized.');
     return _cache;
 })();
+// the cache class will do the following: 
+// reset the key value pair for cache by passing in the default value for that specific key
+class Cache {
+    constructor() {
+        this.cache = cache;
+    }
+    updateCache(key, value) {
+        this.cache.set(key, value);
+    }
+    getCacheVal(key) {
+        return this.cache.get(key);
+    }
+    deleteCacheVal(key) {
+        this.cache.del(key);
+    }
+    resetCacheVal(key, valForReset) {
+        this.cache.set(key, valForReset);
+    }
+}
 export default cache;
--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -8,4 +8,31 @@ const cache = (() => {
     return _cache;
 })();
 
+
+// the cache class will do the following: 
+// reset the key value pair for cache by passing in the default value for that specific key
+class Cache {
+    cache: NodeCache;
+
+    constructor() {
+        this.cache = cache;
+    }
+
+    updateCache(key: string, value: any): void {
+        this.cache.set(key, value)
+    }
+
+    getCacheVal(key: string): unknown {
+        return this.cache.get(key);
+    }
+
+    deleteCacheVal(key: string): void {
+        this.cache.del(key);
+    }
+
+    resetCacheVal(key: string, valForReset: [] | unknown): void {
+        this.cache.set(key, valForReset);
+    }
+}
+
 export default cache;
